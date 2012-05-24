953ee2a6a2661e40291bc8a33356e29de3d30fd3
ceshine <ceshine@ceshine.net>
2012-05-24 17:46:13 +0800

    Numerical Gradient Checking
    Verify the gradient computed by the cost function.

---
+++ checkRRLGradient.m
@@ -0,0 +1,22 @@
+clear
+load('retDAX.txt')
+load('DAX.txt')
+
+M = 10; 
+T = 100; % The number of time series inputs to the trader
+
+X = retDAX(1:M+T); % truncate input data
+
+Xn = featureNormalize(X);
+
+debug_theta = zeros(M+2,1); % initialize theta
+debug_theta = reshape(sin(1:numel(debug_theta)), size(debug_theta));
+
+[J, grad] = costFunction(Xn, X, debug_theta);
+
+costFunc = @(p) costFunction(Xn, X, p);
+
+numgrad = getNumericalGradient(costFunc, debug_theta);
+
+disp([numgrad grad]);
+
